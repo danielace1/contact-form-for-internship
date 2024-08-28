bf37256ddec3c11ddb844a40705087cc62122620
danielace1 <danielsudharsan1805@gmail.com>
2024-08-28 20:33:34 +0530

    fix: server.js

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const app = express();
 
 const PORT = 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", router);
 app.get("/", (req, res) => {
