3d12956148daf32cda7193ad1ad95df547eba7ae
danielace1 <danielsudharsan1805@gmail.com>
2024-08-28 20:04:58 +0530

    feat: added cors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import router from "./route/contact.route.js";
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/", router);
 app.get("/", (req, res) => {
   res.send("Welcome to the API!");
